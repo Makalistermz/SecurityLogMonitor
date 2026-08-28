9981b4884658d41cb690cdd1887e29bec874984b
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-08-28 20:18:22 -0300

    feat: transforma a planilha de logs em array

--- src/reader.js
+++ src/reader.js
@@ -1,4 +1,20 @@
 const fs = require('fs');
 
 const logs = fs.readFileSync('../logs/security-monitor-access.log', 'utf8');
-console.log(logs);
+
+const removerQuebraLinha = logs.split('\n');
+
+function retornarLinhaSemAsterisco(value) {
+    if (value.charAt(0) !== "#") { // verifica se a primeira letra ou simbolo é oque eu passei
+        return value;
+    }
+}
+
+function retornarLinhasSemStringVazia(value) {
+    if(value !== "") {
+        return value;
+    }
+}
+
+const removervazio = removerQuebraLinha.filter(retornarLinhaSemAsterisco).filter(retornarLinhasSemStringVazia);
+console.log(removervazio)
