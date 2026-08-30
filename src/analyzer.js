15c843c79a3c1503eebee610c44ef36bb4863dfb
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-08-30 13:16:57 -0300

    Update analyzer.js

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,7 +1,31 @@
 import { linhaEmObjeto } from "./reader.js";
 
 function detectarForcaBruta() {
-    
+
+    const tentativasPorIp = {};
+
+    linhaEmObjeto.dados.forEach(linha => {
+
+        if (linha.rota === "/login" && linha.status === 401) {
+
+            if (!tentativasPorIp[linha.ip]) {
+                tentativasPorIp[linha.ip] = 0;
+            }
+
+            tentativasPorIp[linha.ip]++;
+        }
+
+    });
+
+    Object.entries(tentativasPorIp).forEach(([ip, quantidade]) => {
+
+        if (quantidade > 3) {
+            console.log(
+                `Possível força bruta do IP: ${ip} - ${quantidade} tentativas`
+            );
+        }
+
+    });
 }
 
-detectarForcaBruta()
+detectarForcaBruta();
